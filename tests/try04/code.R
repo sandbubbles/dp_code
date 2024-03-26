27d54992cf4e6e671de21684af24738b11c85dd8
Karolina Hrncirikova <hrncikar@fit.cvut.cz>
2024-03-26 11:09:38 +0100

    Add example code with function to profile

---
+++ tests/try04/code.R
@@ -0,0 +1,19 @@
+
+foo <- function ( n ) {
+    return (1 + n) * 3 * 4 
+}
+..my_profile.. <- function () {
+    print("f")
+    3 +2
+    { 
+        6 * 3
+        i <- 1 
+        while (i < 1000000) {
+            b <- foo(i) * 3
+            i <- i + 1
+            print(i)
+        }
+        print(b)
+    } 
+}
+{ 1 + 2 ; ..my_profile..() ;print("hi") }
