aef81204f9f5c5c176759899412ed68770b22182
Karolina Hrncirikova <hrncikar@fit.cvut.cz>
2024-04-05 09:53:39 +0200

    Add results on mac

---
+++ tests/try11/code.R
@@ -0,0 +1,11 @@
+..my_profile.. <- function () {
+    for (i in 1:10000) {
+        print("f")
+        3 +2
+        { 
+            6 * 3
+            print("l") 
+        }
+    }
+}
+..my_profile..() 
